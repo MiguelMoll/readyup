9fed525089d905a49d8ceaf701d5e6247e6887a3
Miguel Moll <miguel.moll@gmail.com>
2018-11-19 21:49:47 -0500

    changes to setup ubuntu/debian and redhat/fedora

--- run.sh
+++ run.sh
@@ -1,4 +1,20 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-sudo dnf install -y ansible libselinux-python
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION
+fi
+
+case $OS in
+	Ubuntu)
+		echo "Setting up Ubuntu $VER"
+		sudo apt update && sudo apt install -y ansible
+		;;
+	*)
+		echo "Should be fedora?"
+		#sudo dnf install -y ansible libselinux-python
+		;;
+esac
+
 ansible-playbook -K playbooks/setup.yaml
